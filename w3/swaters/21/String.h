3b1226352babd9c5e7cd5a6626eaa07309220621
Alex Swaters <alex@swaters.net>
2022-12-04 20:31:25 +0100

    20 and minor changes in 18

---
+++ w3/swaters/21/String.h
@@ -0,0 +1,65 @@
+//
+// Created by Alex Swaters on 12/4/2022.
+//
+
+#ifndef INC_21_STRING_H
+#define INC_21_STRING_H
+
+#include <string>
+
+class String : public std::string
+{
+public:
+    String &insert(std::string const &txt);
+
+private:
+    friend bool operator==(String const &lhs, String const &rhs);
+
+    friend bool operator!=(String const &lhs, String const &rhs);
+
+    friend bool operator<(String const &lhs, String const &rhs);
+
+    friend bool operator<=(String const &lhs, String const &rhs);
+
+    friend bool operator>(String const &lhs, String const &rhs);
+
+    friend bool operator>=(String const &lhs, String const &rhs);
+};
+
+inline String &String::insert(std::string const &txt)
+{
+    std::string::insert(0, txt);
+    return *this;
+}
+
+inline bool operator==(String const &lhs, String const &rhs)
+{
+    return static_cast<std::string>(lhs) == static_cast<std::string>(rhs);
+}
+
+inline bool operator!=(String const &lhs, String const &rhs)
+{
+    return !(lhs == rhs);
+}
+
+inline bool operator<(String const &lhs, String const &rhs)
+{
+    return static_cast<std::string>(lhs) < static_cast<std::string>(rhs);
+}
+
+inline bool operator<=(String const &lhs, String const &rhs)
+{
+    return !(rhs < lhs);
+}
+
+inline bool operator>(String const &lhs, String const &rhs)
+{
+    return rhs < lhs;
+}
+
+inline bool operator>=(String const &lhs, String const &rhs)
+{
+    return !(lhs < rhs);
+}
+
+#endif //INC_21_STRING_H
