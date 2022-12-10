28d45bd76617d516bdf567012bfbdd3eb39dcd97
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2022-12-10 02:00:16 +0100

    Set 3 ex18 should be fixed

--- set3_prep/18/base.h
+++ set3_prep/18/base.h
@@ -1,13 +1,25 @@
 #ifndef BASE_H
 #define BASE_H
 
+#include <iostream>
 class Base
 {
 public:
-    Base();
-    Base(Base const &base);
-    Base(Base &&tmp);
-    ~Base();
+    // Following methods define in the header file for simplicity
+    Base()
+    {
+        std::cout << "Base default constructor\n";
+    }
+    ~Base() = default;
+    Base(Base const &other)
+    {
+        std::cout << "Base copy called\n";
+    }
+
+    Base(Base &&tmp)
+    {
+        std::cout << "Base move called\n";
+    }
 };
 
 #endif
--- set3_prep/18/derived.h
+++ set3_prep/18/derived.h
@@ -1,13 +1,24 @@
 #ifndef DERIVED_H
 #define DERIVED_H
 
+#include "base.h"
+#include <iostream>
 class Derived : public Base
 {
 public:
-    Derived();
-    Derived(Derived const &other);
-    Derived(Derived &&tmp);
-    ~Derived();
-};
+    Derived()
+    {
+        std::cout << "Derived default constructor\n";
+    }
 
+    Derived(Derived const &other) : Base(std::move(const_cast<Derived &>(other)))
+    {
+        std::cout << "Derived copy called\n";
+    }
+
+    Derived(Derived &&tmp) : Base(tmp)
+    {
+        std::cout << "Derived move called\n";
+    }
+};
 #endif
