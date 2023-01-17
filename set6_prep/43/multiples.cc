7e502d2dd26e73708b93cf6f37d32208be299f1d
Alex Swaters <alex@swaters.net>
2023-01-17 12:24:46 +0100

    fixes 43 (should not affect behavior)

--- set6_prep/43/multiples.cc
+++ set6_prep/43/multiples.cc
@@ -6,12 +6,12 @@ size_t multiples(std::vector<size_t> const &data)
     size_t res = 0;
 
     std::for_each(tmp.begin(), tmp.end(),
-                  [&](auto a) mutable
-                  {
-                      int count = std::count(tmp.begin(), tmp.end(), a);
-                      if (count > 1)
-                          res += count - 1;
-                      tmp.erase(std::remove(tmp.begin(), tmp.end(), a), tmp.end());
-                  });
+          [&](auto value) mutable
+          {
+              int count = std::count(tmp.begin(), tmp.end(), value);
+              if (count > 1)
+                  res += count - 1;
+              tmp.erase(std::remove(tmp.begin(), tmp.end(), value), tmp.end());
+          });
     return res;
 }
