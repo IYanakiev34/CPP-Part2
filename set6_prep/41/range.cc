04bd7b319d337ef8512dee0b483b1f055f3024c6
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2023-01-17 18:17:46 +0100

    Fixed range.cc for 41 to compare int vs int not unsigned long int to int

--- set6_prep/41/range.cc
+++ set6_prep/41/range.cc
@@ -4,7 +4,7 @@ std::vector<int> Random::range()
 {
     std::vector<int> range;
 
-    for (std::size_t num = d_Dis.min(); num != d_Dis.max(); ++num)
+    for (int num = d_Dis.min(); num != d_Dis.max(); ++num)
         range.push_back(num);
 
     return range;
