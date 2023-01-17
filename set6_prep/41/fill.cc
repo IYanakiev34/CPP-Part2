5054d1e99398090b24da727b8e5703e093b73c5e
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2023-01-17 18:20:37 +0100

    fixed fill.cc 41 comparison between unsigned int and int to unsigned int and unsigned int

--- set6_prep/41/fill.cc
+++ set6_prep/41/fill.cc
@@ -5,6 +5,6 @@ void FillUnique::fill(unsigned int size, unsigned int low, unsigned int high,
                       std::vector<int> &vec)
 {
     Random random(low, high);
-    for (int idx = 0; idx != size; ++idx)
+    for (unsigned int idx = 0; idx != size; ++idx)
         storeValue(vec, random());
 }
