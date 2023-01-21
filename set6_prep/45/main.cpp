9bb9e50bc558c2e5f70341adb64353617cd45408
Alex Swaters <alex@swaters.net>
2023-01-21 17:56:34 +0100

    fix 45

--- set6_prep/45/main.cpp
+++ set6_prep/45/main.cpp
@@ -12,6 +12,6 @@ int main()
      * auto strPtrs = std::unique_ptr<string*[]>(new string*[10]);
      */
     unique_ptr<char[]>{new char[10]}; // NOLINT (don't intend to use)
-    unique_ptr<unique_ptr<string>[]> strPtrs{new unique_ptr<string>[10]};
-    strPtrs[0] = std::make_unique<string>("Hello"); // No leaks
+    unique_ptr<string> strings[10];
+    strings[0] = unique_ptr<string>{new string{"Hello"}};
 }
