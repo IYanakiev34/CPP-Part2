2458d5fb82f28ae23780b7003384a5fbcfabaaec
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2023-01-24 16:37:16 +0100

    fixed casecomp statement on 44

--- set6_prep/44/sort_names.cc
+++ set6_prep/44/sort_names.cc
@@ -6,5 +6,5 @@ void sortByLastNames(std::vector<Student> &students)
     std::sort(students.begin(), students.end(),
               [](Student &student1, Student &student2) -> bool
               {
-                  return (strcasecmp(student1.lastName().c_str(), student2.lastName().c_str())) < 0 ? true : false;
+                  return (strcasecmp(student1.lastName().c_str(), student2.lastName().c_str())) < 0;
               });
